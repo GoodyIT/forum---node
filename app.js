NodeBB/NodeBB

commit aa2af345f9c60824aa4028f98e64fa35633f4a69

removed unused deps

app.js
@@ -25,13 +25,10 @@ nconf.argv().env('__');
 
 var url = require('url'),
 	async = require('async'),
-	semver = require('semver'),
 	winston = require('winston'),
-	colors = require('colors'),
 	path = require('path'),
 	pkg = require('./package.json'),
-	file = require('./src/file'),
-	utils = require('./public/src/utils.js');
+	file = require('./src/file');
 
 global.env = process.env.NODE_ENV || 'production';
 

src/meta/js.js
@@ -4,7 +4,6 @@ var winston = require('winston'),
 	fork = require('child_process').fork,
 	path = require('path'),
 	async = require('async'),
-	_ = require('underscore'),
 	nconf = require('nconf'),
 	fs = require('fs'),
 	file = require('../file'),

GoodyIT/forum---node

commit 16bbe54262f904d39813527ce2e489439dc04445

closes #4303
Resolves regression introduced in aa2af345f9c60824aa4028f98e64fa35633f4a69

app.js
@@ -26,6 +26,7 @@ nconf.argv().env('__');
 var url = require('url'),
 	async = require('async'),
 	winston = require('winston'),
+	colors = require('colors'),
 	path = require('path'),
 	pkg = require('./package.json'),
 	file = require('./src/file');