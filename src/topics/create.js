NodeBB/NodeBB

commit 576df8421895667eb0d088a9f4c3b84df0d66d27

trimming composer input before doing length check in replies

src/topics/create.js
@@ -3,6 +3,7 @@
 
 var async = require('async');
 var validator = require('validator');
+var S = require('string');
 var db = require('../database');
 var utils = require('../../public/src/utils');
 var plugins = require('../plugins');
@@ -327,6 +328,9 @@ module.exports = function (Topics) {
 	}
 
 	function check(item, min, max, minError, maxError, callback) {
+		// Trim and remove HTML (latter for composers that send in HTML, like redactor)
+		item = S(item.trim()).stripTags().s;
+
 		if (!item || item.length < parseInt(min, 10)) {
 			return callback(new Error('[[error:' + minError + ', ' + min + ']]'));
 		} else if (item.length > parseInt(max, 10)) {

GoodyIT/forum---node

commit 088bb8f078b26a25bdf331e8edac5f852eadeb27

fix topic creation regression caused by 576df8421895667eb0d088a9f4c3b84df0d66d27

src/topics/create.js
@@ -329,7 +329,9 @@ module.exports = function (Topics) {
 
 	function check(item, min, max, minError, maxError, callback) {
 		// Trim and remove HTML (latter for composers that send in HTML, like redactor)
-		item = S(item.trim()).stripTags().s;
+		if (typeof item === 'string') {
+			item = S(item.trim()).stripTags().s;
+		}
 
 		if (!item || item.length < parseInt(min, 10)) {
 			return callback(new Error('[[error:' + minError + ', ' + min + ']]'));